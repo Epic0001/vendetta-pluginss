9f2585990158b7b6b6da44726e5830ba098da1c5
Epic <110989057+Epic0001@users.noreply.github.com>
2024-09-13 18:37:06 -0400

    Update test.js

--- plugins/HideMessages/test.js
+++ plugins/HideMessages/test.js
@@ -3,7 +3,7 @@ let authkey;
 
 var userinventory = null;
 var thisusername = "";
-if(loginkey){
+if (typeof loginkey !== 'undefined' && loginkey) {
  authkey = loginkey;
 }else{
 loginkey = prompt("Please your key:");
