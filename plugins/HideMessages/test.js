7ee29cb5ad401ab17a1761a5ebd833b87ec2cc12
Epic <110989057+Epic0001@users.noreply.github.com>
2024-09-15 15:55:57 -0400

    Update test.js

--- plugins/HideMessages/test.js
+++ plugins/HideMessages/test.js
@@ -480,9 +480,9 @@ function OpenTipMenu(username)
     tipbutton.innerHTML = "Tip";
     tipbutton.addEventListener("click", function ()
     {
-        //if (!document.querySelector(".itempreview[selected='true']")) return;
-        websocket.send(JSON.stringify({type: "tip", username: username, skins: [14] }));
-      //  websocket.send(JSON.stringify({type: "tip", username: username, skins: Array.from(document.querySelectorAll(".itempreview[selected='true']")).map(item => item.getAttribute("itemindex"))}));
+        if (!document.querySelector(".itempreview[selected='true']")) return;
+        //websocket.send(JSON.stringify({type: "tip", username: username, skins: [14] }));
+        websocket.send(JSON.stringify({type: "tip", username: username, skins: Array.from(document.querySelectorAll(".itempreview[selected='true']")).map(item => item.getAttribute("itemindex"))}));
         document.querySelector(".panelbackground").remove();
         document.querySelector(".tipmenu").remove();
     });
